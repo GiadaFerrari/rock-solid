a504409cb78ace573ee71d7f11c681b9e79260bb
Giada Ferrari <giadaferrari@Giadas-MBP.fios-router.home>
2019-05-29 12:41:57 -0400

    toggle burger menu works

--- js/global.js
+++ js/global.js
@@ -1,1 +1,27 @@
-let mobileNav;
+let burger = $('[rel="burger"]')
+
+$(document).click(burger, () => {
+    let height = getHeight($('[rel="menu"]'))
+    if (height === 0) {
+        animateMenu(180, 10)
+    } else {
+        animateMenu(0, 0)
+    }
+
+})
+
+function animateMenu(height, padding) {
+    $('[rel="menu"]').animate({
+        'height': height,
+        'padding-top': padding,
+        'padding-bottom': padding
+
+    })
+}
+
+function getHeight(elem) {
+    let height;
+    height = $('[rel="menu"]').outerHeight()
+    console.log(height)
+    return height
+}
