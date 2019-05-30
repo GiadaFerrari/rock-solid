bd84942423f0afad6238800786b1b8fdde099498
Giada Ferrari <giadaferrari@Giadas-MBP.fios-router.home>
2019-05-30 16:12:32 -0400

    about desktop/tablet vs

--- js/about.js
+++ js/about.js
@@ -1,8 +1,3 @@
-//$('.carousel').slick({
-//    infinite: true,
-//    slidesToShow: 1,
-//    slidesToScroll: 1
-//});
 $(document).ready(() => {
     carouselInit()
 })
@@ -11,36 +6,9 @@ function carouselInit() {
     $('.carousel').slick({
         centerMode: true,
         infinite: true,
-        speed: 300,
+        speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-    },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-    },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+        slidesToScroll: 1
+
     });
 }
